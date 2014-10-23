0d82345eb6e3fd28aa7621dd1c5f354a9d054533
repzy <rrepzy@gmail.com>
2014-10-23 15:41:05 +0300

    fix problem zero in the beginning

--- calc.php
+++ calc.php
@@ -4,11 +4,11 @@ include 'Calculator.php';
 
 	if(isset($_POST["number"])){
         $number = $_POST["number"];
-        if (ereg("^[0-9]{0,9}$",$number)){
-
+        if(strlen($number)>1 && $number{0}==0){
+            echo "Incorrect number";
+        }elseif (ereg("^[0-9]{0,9}$",$number)){
             $calc = new Calculator($number);
             echo($calc->choose($number));
-
         }else{
             echo "Incorrect number";
         }
